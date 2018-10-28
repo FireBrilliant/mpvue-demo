a38b63b3c8d3f30a62ab05d747981e69134fbc13
kong <13797469477@163.com>
2018-10-29 00:09:02 +0800

    Initial commit

---
+++ src/pages/todolist/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>vuejs is good</h1>
+  <todolist></todolist>
+</div>
+</template>
+
+<script>
+  import Todolist from '@/components/Todolist'
+  export default {
+    name: 'index',
+    components: {
+      Todolist
+    }
+  }
+</script>
+
+<style scoped>
+h1{
+  color:red;
+}
+</style>
